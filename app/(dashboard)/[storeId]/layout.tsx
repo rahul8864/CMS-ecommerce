fb0b217f0ea537abe12da1eff9b4a6f781473b9d
Rahul Kumar <rahul@aerosimple.in>
2023-07-12 11:10:37 +0530

    CMS Dashboard

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation"
+import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
+
+interface DashboardType {
+    children: React.ReactNode;
+    params: { storeId: string }
+}
+
+export default async function Dashboard({children, params}: DashboardType) {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+
+    const store = await prismadb?.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    })
+
+    if (!store) {
+        redirect('/');
+    }
+
+      const stores = await prismadb?.store.findMany({
+        where: {
+        userId,
+        }
+    })
+
+    console.log(stores)
+
+    return (
+        <>
+            <Navbar stores={stores}/>
+            {children}
+        </>
+    )
+}
